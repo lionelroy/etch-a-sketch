165340b9d428ad8dd0ab9ea4918e663af2224a61
Stephane Roy <stephroy411@hotmail.com>
2021-07-26 14:12:32 -0300

    Added logic for 16x16 grid with hover effect that outputs random RGB color

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+const gridContainer = document.querySelector("#grid-container");
+
+window.addEventListener("load", setDefaultGrid);
+
+function setDefaultGrid() {
+  setGridSize(16);
+  fillGrid(16);
+}
+
+function setGridSize(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+function fillGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList = "grid-element";
+    gridElement.addEventListener("mouseover", changeColor);
+    gridContainer.appendChild(gridElement);
+  }
+}
+
+function changeColor(e) {
+  const randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
+
